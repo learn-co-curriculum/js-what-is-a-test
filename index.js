8f67a0178cee1fa19fc7765da9b14202ecef543d
Oliver Bebber <ogbebber@gmail.com>
2020-07-23 22:39:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = `${name} is ${height} inches tall`
-
-module.exports = {name, height, message}
+var name = "Oliver";
+var height = 60;
+var message = `${name} is ${height} inches tall`;
+console.log(message);
+module.exports = { name, height, message };
